03187211c53544cf9470c3eb749eaf3cfebfac80
nasimGn <you@example.com>
2019-09-03 15:09:40 +0430

    add additional button

---
+++ src/components/test.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class TestButton extends Component {
+  state = {};
+  render() {
+    return <button className="btn btn-danger">Delete</button>;
+  }
+}
+
+export default TestButton;
